98706a5f7f8e6cb182d3358117fdd1a0f7e0c274
hongze <hongzecheung@gmail.com>
2026-08-21 14:18:46 +0800

    1

--- backend/app/main.py
+++ backend/app/main.py
@@ -16,8 +16,12 @@ app = FastAPI(
 )
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=settings.cors_origins,
-    allow_credentials=False,
+    allow_origins=[
+        "https://mingshu.cloud",
+        "https://www.mingshu.cloud",
+        "http://localhost:5173",
+    ],
+    allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
